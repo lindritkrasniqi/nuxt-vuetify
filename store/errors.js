a02a9e3445bca0f935a283b342cca3330971b879
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-27 10:38:30 +0200

    Menage errors with error module

---
+++ store/errors.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+  message: null,
+  error: []
+});
+
+export const mutations = {
+  error: (state, data) => {
+    state.message = data.message ?? null;
+    state.error = data.errors ?? [];
+  }
+};
